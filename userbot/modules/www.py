b000c4a952e09f25ac9123e6eac6762cc388288e
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2019-12-14 14:50:05 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -17,7 +17,7 @@ from userbot.events import register
 @register(outgoing=True, pattern="^.speed$")
 async def speedtst(spd):
     """ For .speed command, use SpeedTest to check server speeds. """
-    await spd.edit("`Running speed test . . .`")
+    await spd.edit("Downloading Maria Ozawa...")
     test = Speedtest()
 
     test.get_best_server()
@@ -26,8 +26,7 @@ async def speedtst(spd):
     test.results.share()
     result = test.results.dict()
 
-    await spd.edit("`"
-                   "Started at "
+    await spd.edit("Started at "
                    f"{result['timestamp']} \n\n"
                    "Download "
                    f"{speed_convert(result['download'])} \n"
@@ -37,7 +36,7 @@ async def speedtst(spd):
                    f"{result['ping']} \n"
                    "ISP "
                    f"{result['client']['isp']}"
-                   "`")
+                   )
 
 
 def speed_convert(size):
@@ -57,9 +56,9 @@ def speed_convert(size):
 async def neardc(event):
     """ For .dc command, get the nearest datacenter information. """
     result = await event.client(functions.help.GetNearestDcRequest())
-    await event.edit(f"Country : `{result.country}`\n"
-                     f"Nearest Datacenter : `{result.nearest_dc}`\n"
-                     f"This Datacenter : `{result.this_dc}`")
+    await event.edit(f"Country : {result.country}\n"
+                     f"Nearest Datacenter : {result.nearest_dc}\n"
+                     f"This Datacenter : {result.this_dc}")
 
 
 @register(outgoing=True, pattern="^.p$")
@@ -69,7 +68,7 @@ async def pingme(pong):
     await pong.edit("`Pong!`")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit("`Pong!\n%sms`" % (duration))
+    await pong.edit("Ping Pong !!! %sms" % (duration))
 
 
 """CMD_HELP.update(
