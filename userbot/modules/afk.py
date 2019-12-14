f1ef29726924444b5c44ea916f765e4b82e51000
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2019-12-14 17:09:09 +0700

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -161,10 +161,10 @@ async def type_afk_is_not_true(notafk):
         AFKREASON = None
 
 
-CMD_HELP.update({
+"""CMD_HELP.update({
     "afk":
     ".afk [Optional Reason]\
 \nUsage: Sets you as afk.\nReplies to anyone who tags/PM's \
 you telling them that you are AFK(reason).\n\nSwitches off AFK when you type back anything, anywhere.\
 "
-})
+})"""
