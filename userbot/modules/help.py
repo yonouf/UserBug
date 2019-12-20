d8551c7ea61dad77de71d5018cbd467063938fa5
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2019-12-20 13:35:44 +0000

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -22,6 +22,6 @@ async def help(event):
         await event.edit(".h <module name> for Detail or .<module> Retrieves All Available CMD.")
         string = ""
         for i in CMD_HELP:
-            string += (".h <module name> for Detail or .<module> Retrieves All Available CMD.\n") + str(i)
+            string += "" + str(i)
             string += " ⊙ "
         await event.edit(string)
