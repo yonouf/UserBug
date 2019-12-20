73af7e5482bb73ab48f7347b8396976db965628b
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2019-12-20 11:26:51 +0000

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -15,7 +15,7 @@ async def help(event):
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await event.edit(".h <module name> for Detail or .<module> Retrieves All Available CMD."(CMD_HELP[args]))
+            await event.edit(str(CMD_HELP[args]))
         else:
             await event.edit("Invalid CMD...!!! Are u Blind ???")
     else:
