1ff7dce2b9aa76b5deceb9990422f3521ca9f091
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2019-12-20 16:16:01 +0000

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -147,7 +147,7 @@ async def gdrive_upload_function(dryb):
                                              file_name, mime_type, dryb,
                                              parent_id)
             await dryb.edit(
-                f"File: {required_file_name} Uploaded to [𝐆𝐃𝐑𝐈𝐕𝐄]({g_drive_link})!"
+                f"[This files]({required_file_name}) Uploaded to [𝐆𝐃𝐑𝐈𝐕𝐄]({g_drive_link})!"
             )
         except Exception as e:
             await dryb.edit(
