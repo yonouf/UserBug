a39031acbf74e3d40cdf50d0fe1c31cf24c828ea
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2019-12-20 19:46:07 +0700

    Update ocr.py

--- userbot/modules/ocr.py
+++ userbot/modules/ocr.py
@@ -44,7 +44,7 @@ async def ocr_space_file(filename,
 
 @register(pattern=r".oc (.*)", outgoing=True)
 async def ocr(event):
-    await event.edit("`Reading...`")
+    await event.edit("Reading...")
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
     lang_code = event.pattern_match.group(1)
@@ -55,9 +55,9 @@ async def ocr(event):
     try:
         ParsedText = test_file["ParsedResults"][0]["ParsedText"]
     except BaseException:
-        await event.edit("`Couldn't read it.`\n`I guess I need new glasses.`")
+        await event.edit("Couldn't read it.")
     else:
-        await event.edit(f"`Here's what I could read from it:`\n\n{ParsedText}"
+        await event.edit(f"Here's what I could read from it:`\n{ParsedText}"
                          )
     os.remove(downloaded_file_name)
 
