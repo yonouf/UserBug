bfa516f500d7c75396ae33b6c6abff5a87c31328
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2019-12-25 19:31:35 +0700

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -915,13 +915,14 @@ async def get_bots(show):
 
 @register(outgoing=True, pattern="^.admin$")
 async def dumper(dumber):
-    await dumber.edit("⊙ 𝐀𝐃𝐌𝐈𝐍 ⊙ :\n⊙ .k ⊙ .b ⊙ .ub ⊙ .pin ⊙ .m ⊙ .um ⊙ .lo\n⊙ .ul ⊙ .pro ⊙ .dem ⊙ .zo ⊙ .ads ⊙ .uss\n⊙ Help : .h 𝐀𝐃𝐌𝐈𝐍 for Details.")
+    await dumber.edit("⊙ 𝐀𝐃𝐌𝐈𝐍 ⊙ :\n⊙ .k ⊙ .b ⊙ .ub ⊙ .pin ⊙ .m ⊙ .um ⊙ .lo ⊙\n⊙ .ul ⊙ .pro ⊙ .dem ⊙ .zo ⊙ .ads ⊙ .us ⊙\n⊙ Help : .h 𝐀𝐃𝐌𝐈𝐍 for Details ⊙")
 
 CMD_HELP.update({
     "admin":
-	"⊙ 𝐀𝐃𝐌𝐈𝐍 ⊙ :\n⊙ .ads .us : Admins & Users Lists ⊙ .b .ub : Ban & Unban\
-	\n⊙ .k : Kick ⊙ .pin : Pin Messages ⊙ .m .um : Mute & Unmute\
-	\n⊙ .pro .dem : Promote & Demote ⊙ .zo : Scan & Clean Zombies\
-	\n⊙ .lo .ul : Lock & Unlock Types : all, msg, media, sticker, gif, game,\
-	\ninline, poll, invite, pin, info."
+	"⊙ 𝐀𝐃𝐌𝐈𝐍 ⊙ :\
+	\n⊙ .ads .us : Admins & Users Lists ⊙ .b .ub : Ban & Unban ⊙\
+	\n⊙ .k : Kick ⊙ .pin : Pin Messages ⊙ .m .um : Mute & Unmute ⊙\
+	\n⊙ .pro .dem : Promote & Demote ⊙ .zo : Scan & Clean Zombies ⊙\
+	\n⊙ .lo .ul : Lock & Unlock ⊙ Types : all, msg, media, sticker, gif, game,\
+	\ninline, poll, invite, pin, info ⊙"
 })
