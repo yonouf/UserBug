b0bbc094d64cf64b9b0e98e26819e5b930e29aea
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2019-12-31 16:15:50 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -94,7 +94,7 @@ CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
 GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
 
 #Telegraph
-TELEGRAPHSHORTNAME = os.environ.get("TELEGRAPHSHORTNAME", None)
+TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", None)
 
 # OpenWeatherMap API Key
 OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
